958ec2a3c91169995c4f2ee279ab419fa190491d
BORISH1 <borishningombam@gmail.com>
2025-03-12 15:42:33 +0530

    string concate

---
+++ String_builder/concate.java
@@ -0,0 +1,13 @@
+
+
+public class concate {
+    void concat(String a1,String a2){
+ String a3 = a1 + a2;
+ System.out.println(a3);
+    }
+    public static void main(String[] args) {
+       concate obj = new concate();
+        obj.concat("Ningombam","Borish");
+      
+    }
+}
