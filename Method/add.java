a24fd4183ced4d557c9973664e2ee9fb04d75f98
BORISH1 <borishningombam@gmail.com>
2025-03-12 14:37:11 +0530

    method program

---
+++ Method/add.java
@@ -0,0 +1,15 @@
+package Method;
+
+public class add{
+    public int addition(int a, int b)
+{
+     int c = a+b;
+    return c;
+}   
+public static void main(String[] args) {
+    add obj = new add();
+    int k = obj.addition(12, 034);
+    System.out.println(k);
+}
+    
+}
