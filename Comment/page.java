718e3903c3b8e823d8a900f2d68ef63eaf407a20
BORISH1 <borishningombam@gmail.com>
2025-03-12 14:13:17 +0530

    comment

---
+++ Comment/page.java
@@ -0,0 +1,11 @@
+package Comment;
+
+public class page {
+    public static void main(String args[]) {
+        int num = 12;// the value of num is 12 
+        System.out.println(num); //this is used to print the num 
+        /*this is the multi comment line 
+         * we can write many comment using this
+         */
+    }
+}
