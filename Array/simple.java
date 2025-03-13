3056b81c0210c90b7777d185c3c74d03d0db060f
BORISH1 <borishningombam@gmail.com>
2025-03-13 12:09:50 +0530

    array simple program

---
+++ Array/simple.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class simple {
+    public static void main(String[] args) {
+        int a[] ={23,34,45,67,65};
+        System.out.println(a[0]);
+        System.out.println(a[4]);
+    }
+}
