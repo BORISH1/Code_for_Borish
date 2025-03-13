dc72c7026ba8b0547a555ae3efe88ef007498550
BORISH1 <borishningombam@gmail.com>
2025-03-13 10:51:32 +0530

    simple if statement boolean program

--- Conditional_operator/simple.java
+++ Conditional_operator/simple.java
@@ -14,5 +14,6 @@ public class simple {
         System.out.println(d);
         System.out.println(e);
         System.out.println(f);
+         System.out.println(a);
     }
 }
---
+++ conditional_statement/if_condition_boolean_value/simple.java
@@ -0,0 +1,12 @@
+
+
+public class simple {
+    public static void main(String[] args) {
+        if(true){
+                System.out.println("i am borish");
+        }
+        if(false){
+            System.out.println("i don't know");
+        }
+    }
+}
