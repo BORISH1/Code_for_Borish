bc122f0da98a8e32519db58b48a817a10877840b
BORISH1 <borishningombam@gmail.com>
2025-03-13 10:33:04 +0530

    conditional operator simple

---
+++ Conditional_operator/simple.java
@@ -0,0 +1,18 @@
+package Contional_operator;
+
+public class simple {
+    public static void main(String[] args) {
+        boolean a = (23>56);
+        boolean b = (34<56);
+        boolean c =(45>=56);
+        boolean d=(43<=45);
+        boolean e =(45 == 56);
+        boolean f=(45!=56);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
