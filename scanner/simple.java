6938f57150244feaca763eddc5b94774f3473ff2
BORISH1 <borishningombam@gmail.com>
2025-03-13 14:47:26 +0530

    scanner first program

---
+++ scanner/simple.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class simple {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.println("enter your name");
+        String nam = name.nextLine();
+        System.out.println("my name is " + nam);
+    }
+}
