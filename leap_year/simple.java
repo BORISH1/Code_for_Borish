7ad4de055b1f98fb27da422fd9402163a8633d56
BORISH1 <borishningombam@gmail.com>
2025-03-13 14:34:17 +0530

    simple program

---
+++ leap_year/simple.java
@@ -0,0 +1,15 @@
+package leap_year;
+
+public class simple {
+    public static void main(String[] args) {
+        int year = 2004;
+    if(year%4==0)
+    {
+        System.out.println(year +" is leap year");
+    }
+    else{
+        System.out.println(year +" is not leap year");
+    }
+    }
+    
+}
---
+++ range_count/simple.java
@@ -0,0 +1,11 @@
+public class simple {
+    public static void main(String[] args) {
+        int total =0;
+        for(int i = 2020;i<=2030;i++){
+            if(i%4==0){
+                total = total +1;
+            }
+        }
+        System.out.println(total + " is total no. of leap year");
+    }
+}
