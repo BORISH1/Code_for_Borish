5f5f129000b3ce53c9871647a7ebd6666d1155d1
BORISH1 <borishningombam@gmail.com>
2025-03-13 12:17:16 +0530

    ball_selector simple program

---
+++ ball_program/simple.java
@@ -0,0 +1,9 @@
+package ball_program;
+
+public class simple {
+    public static void main(String[] args) {
+         String a[] ={"green","blue","red"};
+    System.out.println(a[2]);
+    }
+   
+}
