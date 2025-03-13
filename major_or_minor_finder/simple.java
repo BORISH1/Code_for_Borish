9dd3538c90b78a425de0565648813515362a3f2a
BORISH1 <borishningombam@gmail.com>
2025-03-13 11:06:36 +0530

    major minor finder

---
+++ major_or_minor_finder/simple.java
@@ -0,0 +1,13 @@
+package major_or_minor_finder;
+
+public class simple {
+    public static void main(String args[]){
+        int age = 45;
+        if(age>18){
+            System.out.println("major");
+        }
+        else{
+            System.out.println("minor");
+        }
+    }
+}
