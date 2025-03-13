5eeadcf68118d3b722b11f03a74e8ecfa3693aef
BORISH1 <borishningombam@gmail.com>
2025-03-13 10:58:18 +0530

    it is the if else statement simple program

---
+++ conditional_statement/if_else_statement/simiple.java
@@ -0,0 +1,14 @@
+package conditional_statement.if_else_statement;
+
+public class simiple {
+    public static void main(String[] args) {
+         if(true){
+        System.out.println("hi how are you?");
+    }
+    else{
+        System.out.println("i am a student");
+    }
+}
+
+    }
+   
