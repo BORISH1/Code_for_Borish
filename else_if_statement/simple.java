4133b27a594b9a56897d510b3a63cfa67236d455
BORISH1 <borishningombam@gmail.com>
2025-03-13 11:45:41 +0530

    else if simple program

---
+++ else_if_statement/simple.java
@@ -0,0 +1,16 @@
+package else_if_statement;
+
+public class simple {
+    public static void main(String[] args) {
+        int a =0;
+        if(a>0){
+            System.out.println("positive");
+        }
+        else if(a<0){
+            System.out.println("negative");;
+        }
+        else{
+            System.out.print("the no. is zero");
+        }
+    }
+}
