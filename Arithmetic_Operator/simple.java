99a82b9ea9b8077e5b261fec3cf4ecc92bed3514
BORISH1 <borishningombam@gmail.com>
2025-03-13 10:20:06 +0530

    arithmetic operator simple program

---
+++ Arithmetic_Operator/simple.java
@@ -0,0 +1,12 @@
+package Arithmetic_Operator;
+
+public class simple {
+    public static void main(String[] args) {
+        System.out.println(10+56);
+        System.out.println(100-56);
+        System.out.println(10*56);
+        System.out.println(100/50);
+        System.out.println(100%56);
+
+    }
+}
