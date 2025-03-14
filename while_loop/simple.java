4f44046e66b31a89177235c7ba833c55877377a5
BORISH1 <borishningombam@gmail.com>
2025-03-14 09:59:13 +0530

    simple while loop

---
+++ while_loop/simple.java
@@ -0,0 +1,11 @@
+package while_loop;
+
+public class simple {
+    public static void main(String[] args) {
+        int i = 92;
+        while(i<=98){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
