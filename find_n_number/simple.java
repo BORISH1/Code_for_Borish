ac8f2599b10932d9427c6d24f9e333bc8c192c3a
BORISH1 <borishningombam@gmail.com>
2025-03-14 10:14:06 +0530

    find n number

---
+++ find_n_number/simple.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+import java.util.Scanner;
+
+public class simple {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int a = obj.nextInt();
+        int i = 1;
+        while(i<=a){
+
+            System.out.println(i);
+            i++;
+        }
+    }
+}
