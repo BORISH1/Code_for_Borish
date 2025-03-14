fa1565586b395e4c6553a4ccc55176f1bdf0c01f
BORISH1 <borishningombam@gmail.com>
2025-03-14 12:00:11 +0530

    this is the case chinging code

---
+++ case_change/simple.java
@@ -0,0 +1,9 @@
+package case_change;
+
+public class simple {
+    public static void main(String[] args) {
+        String a = "Ningombm Borish";
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
+    }
+}
